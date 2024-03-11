3d461111a711342bd10014c0cfd01bf0617604f1
fnzyyy8 <farhanshidayat@gmail.com>
2024-03-11 11:47:57 +0700

    Contol Library Composer

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BelajarComposerLibrary;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+    public function sayName(string $name): void
+    {
+        
+    }
+}
